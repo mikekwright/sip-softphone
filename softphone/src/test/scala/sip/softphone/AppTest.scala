db6d2fc186f7e7d1bbcd4f485e472f73cda3fc38
Michael Wright <mkwright@gmail.com>
2016-10-27 08:30:48 -0600

    Starting to get gradle to work
    Added the gradle changes to get the call ./gradlew to start working
    
    Added a few changes to get the gradle setup working
    
    Added changes to wercker to get it to build (hopefully)
    
    Attempting to get gradle and scalatest working with jacoco
    
    Hopefully getting jacoco xml to output for codecov
    
    Added the codecoverage to the test step
    
    Added more details around the README and codecov

---
+++ softphone/src/test/scala/sip/softphone/AppTest.scala
@@ -0,0 +1,12 @@
+package sip.softphone
+
+import org.scalatest.FunSuite
+
+//import org.scalatest.junit.JUnitRunner
+
+//@RunWith(classOf[JUnitRunner])
+class AppTest extends FunSuite {
+  test("Very Basic") {
+    assert(1 == 1)
+  }
+}
